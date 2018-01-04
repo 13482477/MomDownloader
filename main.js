cf901e169efb4727fd557de6aec27916c15cc86e
liuxin <private2099@163.com>
2018-01-04 11:16:35 +0800

    1.0 finish

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const path = require('path')
 const url = require('url')
 
 const request = require('request')
+const cheerio = require('cheerio')
+
 
 // 保持一个对于 window 对象的全局引用，如果你不这样做，
 // 当 JavaScript 对象被垃圾回收， window 会被自动地关闭
@@ -20,7 +22,7 @@ function createWindow() {
     }))
 
     // 打开开发者工具。
-    win.webContents.openDevTools()
+    //win.webContents.openDevTools()
 
     // 当 window 被关闭，这个事件会被触发。
     win.on('closed', () => {
@@ -28,7 +30,8 @@ function createWindow() {
         // 通常会把多个 window 对象存放在一个数组里面，
         // 与此同时，你应该删除相应的元素。
         win = null
-    })
+    }
+    )
 }
 
 // Electron 会在初始化后并准备
@@ -40,32 +43,50 @@ app.on('ready', createWindow)
 app.on('window-all-closed', () => {
     // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
     // 否则绝大部分应用及其菜单栏会保持激活。
-    if (process.platform !== 'darwin') {
+    if (process.platform !== 'darwin'
+    ) {
         app.quit()
     }
 })
 
 app.on('activate', () => {
-    // 在macOS上，当单击dock图标并且没有其他窗口打开时，
-    // 通常在应用程序中重新创建一个窗口。
     if (win === null) {
         createWindow()
     }
 })
 
-ipcMain.on('searchMusic', (event, arg) => {
-    console.log(arg)
-    let requestURL = `http://www.chedvd.com/so.html?k=${arg}`;
+//搜索歌曲
+ipcMain.on('searchMusic', async (event, arg) => {
+    let requestURL = encodeURI(`http://www.chedvd.com/so.html?k=${arg}`);
+    let result = "<table class='gridtable'><tr><th>歌曲名称</th><th>歌手</th><th>下载</th></tr>";
+    request(requestURL, (error, response, body) => {
+        // if (error) {
 
-    console.log(requestURL)
+        // }
+        // if (response.statusCode != 200) {
 
-    request(requestURL, function (error, response, body) {
-        console.log('error:', error);
-        console.log('statusCode:', response && response.statusCode);
-        console.log('body:', body);
+        // }
+        const $ = cheerio.load(body);
+        $('td a').map((i, element) => {
+            let link = element.attribs.href;
+            if (link.startsWith('/song')) {
+                result = result + "<tr>"
+                result = result + "<td>" + element.childNodes[0].data + "</td>";
+                result = result + "<td>" + $('td a')[i + 1].childNodes[0].data + "</td>";
+                result = result + `<td><button onclick="downloadMusic('${element.attribs.href}')">下载</button></td>`
+                result = result + "</tr>"
+            }
+        });
+        event.returnValue = result;
+    });
+});
+
+ipcMain.on('downloadMusic', async (event, arg) => {
+    let requestURL = encodeURI(`http://www.chedvd.com${arg}`);
+    request(requestURL, (error, response, body) => {
+        const $ = cheerio.load(body);
+        let href = $('div .button a')[0].attribs.href;
+        event.returnValue = `http://www.chedvd.com${href}`;
     });
 
-    event.returnValue = 'pong'
 })
-// 在这文件，你可以续写应用剩下主进程代码。
-// 也可以拆分成几个文件，然后用 require 导入。
